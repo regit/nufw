047f23d646dc3dc96d81c48451d4ffc682b8c189
Vincent Deffontaines <vdeffontaines@inl.fr>
2003-09-15 22:20:29 +0000

    nufw/ restructured

---
+++ src/nufw/authsrv.c
@@ -0,0 +1,122 @@
+
+/*
+** Copyright (C) 2002, Éric Leblond <eric@regit.org>
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation, version 2 of the License.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <structure.h>
+
+
+static void 
+bail (const char *on_what){
+  perror(on_what);
+  exit(1);
+}
+
+
+void* authsrv(){
+  int z;
+  int sck_inet;
+  struct sockaddr_in addr_inet,addr_clnt;
+  int len_inet;
+  char dgram[512];
+
+  //open the socket
+  sck_inet = socket (AF_INET,SOCK_DGRAM,0);
+
+  if (sck_inet == -1)
+    bail("socket()");
+	
+  memset(&addr_inet,0,sizeof addr_inet);
+
+  addr_inet.sin_family= AF_INET;
+  addr_inet.sin_port=htons(authsrv_port);
+  addr_inet.sin_addr.s_addr=INADDR_ANY;
+
+  len_inet = sizeof addr_inet;
+
+  z = bind (sck_inet,
+	    (struct sockaddr *)&addr_inet,
+	    len_inet);
+  if (z == -1)
+    bail ("bind()");
+
+	
+  for(;;){
+    len_inet = sizeof addr_clnt;
+    z = recvfrom(sck_inet,
+		 dgram,
+		 sizeof dgram,
+		 0,
+		 (struct sockaddr *)&addr_clnt,
+		 &len_inet);
+    if (z<0)
+      bail("recvfrom()");
+    //	pckt_rx++;
+    // decode packet
+    auth_packet_to_decision(dgram);
+  }
+  close(sck_inet);
+
+  return NULL;
+}
+
+
+int auth_packet_to_decision(char* dgram){
+  unsigned long packet_id;
+  int sandf;
+  switch (*dgram) {
+  case 0x1:
+    if ( *(dgram+1) == AUTH_ANSWER) {
+      packet_id=*(unsigned long *)(dgram+8);
+      /* lock mutex */
+      pthread_mutex_lock(&packets_list_mutex);
+      sandf=psearch_and_destroy (packet_id);
+      pthread_mutex_unlock(&packets_list_mutex);
+      if (sandf){
+	if ( *(dgram+4) == OK ) {
+	  /* TODO : test on return */
+	  /*if (debug){
+	    printf ("Accepting %lu\n",packet_id);
+	  }
+	  */
+	  /*nufw_debug(1,1,"Accepting\n");*/
+	  IPQ_SET_VERDICT(packet_id, NF_ACCEPT);
+	  pckt_tx++;
+	  return 1;
+	} else {
+	   if (debug){
+	    printf ("Rejecting %lu\n",packet_id);
+	  }
+	  IPQ_SET_VERDICT(packet_id, NF_DROP);
+	  return 0;
+	}
+      } else {
+	if (debug) {
+	  fprintf(stdout,"Packet without a known ID :-(\n");
+	}
+      }
+    } else {
+      if (debug) {
+	fprintf(stdout,"Type %d for packet %lu (not for me)\n",*(dgram+1),*(unsigned long * )(dgram+4));
+      }
+    }
+    break;
+  }
+  return 1;
+}
---
+++ src/nufw/main.c
@@ -0,0 +1,166 @@
+/* $Id: main.c,v 1.1 2003/09/15 22:20:29 gryzor Exp $ */
+
+/*
+** Copyright (C) 2002 Eric Leblond <eric@regit.org>
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation, version 2 of the License.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+*/
+
+
+
+#include <linux/netfilter.h>
+#include <libipq/libipq.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <netdb.h>
+#include <structure.h>
+
+
+int main(int argc,char * argv[]){
+  pthread_t pckt_server,auth_server;
+  struct hostent *authreq_srv;
+  /* option */
+  char * options_list = "DhVvl:d:p:t:T:";
+  int option,daemonize = 0;
+  int value;
+  unsigned int ident_srv;
+  char* version=VERSION;
+
+  /* initialize variables */
+
+  authreq_port = AUTHREQ_PORT;
+  authsrv_port = AUTHSRV_PORT;
+  packet_timeout = PACKET_TIMEOUT;
+  track_size = TRACK_SIZE;
+  id_srv = ID_SERVER;
+  strncpy(authreq_addr,AUTHREQ_ADDR,HOSTNAME_SIZE);
+  debug=DEBUG;
+  pid_t pidf;
+
+  /*parse options */
+  while((option = getopt ( argc, argv, options_list)) != -1 ){
+    switch (option){
+    case 'V' :
+      fprintf (stdout, "PACKAGE (version %s)\n",version);
+      return 1;
+    case 'D' :
+      daemonize = 1;
+      break;
+    case 'v' :
+      fprintf (stdout, "Debug should be On\n");
+      debug=1;
+      break;
+      /* port we listen for auth answer */
+    case 'l' :
+      sscanf(optarg,"%d",&value);
+      printf("Listen on UDP port %d\n",value);
+      authsrv_port=value;
+      break;
+      /* destination port */
+    case 'p' :
+      sscanf(optarg,"%d",&value);
+      printf("Auth request send to port %d\n",value);
+      authreq_port=value;
+      break;
+      /* destination IP */
+    case 'd' :
+      strncpy(authreq_addr,optarg,HOSTNAME_SIZE);
+      printf("Sending Auth request to %s\n",authreq_addr);
+      break;
+      /* packet timeout */
+    case 't' :
+      sscanf(optarg,"%d",&packet_timeout);
+      break;
+      /* max size of packet list */
+    case 'T' :
+      sscanf(optarg,"%d",&track_size);
+      break;
+    case 'I' :
+      sscanf(optarg,"%ud",&ident_srv);
+      id_srv=ident_srv;
+      break;
+    case 'h' :
+      fprintf (stdout ,"PACKAGE [-hVv] [-l local_port] [-d remote_addr] [-p remote_port]  [-t packet_timeout] [-T track_size] [-I id_server]\n");
+      return 1;
+    }
+  }
+
+/* Daemon code */
+if (daemonize == 1) {
+  if ((pidf = fork()) < 0){
+  	printf("Unable to fork\n");
+	exit (-1);
+  } else {
+  	/* parent */
+ 	 if (pidf > 0) {
+		exit(0);
+	}
+  }
+}
+  
+  /* create socket for sending auth request */
+  sck_auth_request = socket (AF_INET,SOCK_DGRAM,0);
+    
+  if (sck_auth_request == -1)
+    printf("socket()");
+     
+  memset(&adr_srv,0,sizeof adr_srv);
+
+  adr_srv.sin_family= AF_INET;
+  adr_srv.sin_port=htons(authreq_port);
+  /* hostname conversion */
+  authreq_srv=gethostbyname(authreq_addr);
+  adr_srv.sin_addr=*(struct in_addr *)authreq_srv->h_addr;
+
+  if (adr_srv.sin_addr.s_addr == INADDR_NONE )
+    fprintf(stdout,"Bad Address.");
+    
+  packets_list_start=NULL;
+  packets_list_end=NULL;
+  packets_list_length=0;
+  /* initialize mutex */
+  pthread_mutex_init(&packets_list_mutex ,NULL);
+
+  /* init netlink connection */
+  hndl = ipq_create_handle(0,PF_INET);
+  if (hndl)
+    ipq_set_mode(hndl, IPQ_COPY_PACKET,BUFSIZ);  
+  else {
+    fprintf (stderr,"Can not create ipq handle\n");
+  }
+
+  /* create thread for packet server */
+  if (pthread_create(&pckt_server,NULL,packetsrv,NULL) == EAGAIN){
+    exit(1);
+  }
+  /* create thread for auth server */
+  if (pthread_create(&auth_server,NULL,authsrv,NULL) == EAGAIN){
+    exit(1);
+  }
+    
+  /* control stuff */
+  pckt_tx=pckt_rx=0;
+  for(;;){
+     pthread_mutex_lock(&packets_list_mutex);
+     clean_old_packets ();
+     pthread_mutex_unlock(&packets_list_mutex);
+    if (debug) {
+      fprintf(stdout,"rx : %d, tx : %d, track_size : %d, start_list : %p\n",pckt_rx,pckt_tx,packets_list_length,packets_list_start);
+    }
+    sleep(5);	
+  }
+}
