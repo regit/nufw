18b85b46157d134b3595203b800c2c591585d9a0
Eric Leblond <eleblond@inl.fr>
2005-11-06 13:00:39 +0000

    r1304@sekspyz:  regit | 2005-11-06 14:04:53 +0100  add netinet/in.h include for proto def

--- src/nutrackd/nutrackd.h
+++ src/nutrackd/nutrackd.h
@@ -6,12 +6,9 @@
 #include <syslog.h>
 #include <signal.h>
 #include <errno.h>
+#include <netinet/in.h>
 #include <libnetfilter_conntrack/libnetfilter_conntrack.h>
 
 #include "nutrackd_debug.h"
-
-//Not clever to redefine these here.
-#define TCP 6
-#define UDP 17
 
 #define PACKET_TIMEOUT 15
