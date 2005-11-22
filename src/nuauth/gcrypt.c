5a6cbf3a5bef35f1e9c0ec2a2d0ea00e20e43a4b
Eric Leblond <eleblond@inl.fr>
2005-11-22 09:08:59 +0000

    changelog has been moved

--- src/nuauth/gcrypt.c
+++ src/nuauth/gcrypt.c
@@ -1,6 +1,6 @@
 /*
  ** Copyright(C) 2004 Eric Leblond <regit@inl.fr>
- **              2005 : deal with seeded/unseeded cases. Patch from Julian Reich <jreich@epplehaus.de>
+ **
  **
  ** This program is free software; you can redistribute it and/or modify
  ** it under the terms of the GNU General Public License as published by
@@ -14,6 +14,10 @@
  ** You should have received a copy of the GNU General Public License
  ** along with this program; if not, write to the Free Software
  ** Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ **
+ ** Changelog :
+ **  20/06/2005 : deal with seeded/unseeded cases. Patch from Julian Reich <jreich@epplehaus.de>
+ **
  */
 #include <auth_srv.h>
 #include <tls.h>
