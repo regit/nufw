4c4dbf36940e53273326033cc2c00302919871a2
Victor Stinner <victor.stinner@inl.fr>
2006-03-02 14:51:48 +0000

    * Create ./configure new option: --with-perf-display (benchmark user    authentification)  * ./autogen.sh script is more verbose

--- autogen.sh
+++ autogen.sh
@@ -1,7 +1,20 @@
 #!/bin/sh
 
 libtoolize --force --automake
+
+echo "Run aclocal"
 aclocal -I macros/
+
+echo "Run autoheader"
 autoheader
+
+echo "Run autoconf"
 autoconf
+
+echo "Run automake"
 automake -a --copy
+
+echo "Done."
+echo
+echo "Now type: ./configure"
+echo "Help with: ./configure --help"
