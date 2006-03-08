4f192a15adc2b4571f0736f6278ad73b86f45471
Victor Stinner <victor.stinner@inl.fr>
2006-03-08 16:48:33 +0000

    Ooops, forget an argument.

--- src/nuauth/modules/log_mysql/mysql.c
+++ src/nuauth/modules/log_mysql/mysql.c
@@ -553,6 +553,7 @@ G_MODULE_EXPORT int user_session_logs(user_session *c_session, session_state_t s
                     "INSERT INTO %s (ip_saddr,start_timestamp) "
                     "VALUES ('%s',%u,FROM_UNIXTIME(%lu))",
                     mysql_users_table_name,
+                    c_session->userid,
                     c_session->addr,
                     time(NULL));
             break;
@@ -583,4 +584,5 @@ G_MODULE_EXPORT int user_session_logs(user_session *c_session, session_state_t s
     }
     return 1;
 }
+
 
