29502eff42d83e5b2b3645dcb669fb1ac583fad7
Victor Stinner <victor.stinner@inl.fr>
2006-06-01 13:33:49 +0000

    Fix english syntax / spelling (address and not adress)

--- src/nufw/main.c
+++ src/nufw/main.c
@@ -489,26 +489,26 @@ int main(int argc,char * argv[])
  
           case 'h' :
             fprintf (stdout ,"%s [-hVcCMv[v[v[v[v[v[v[v[v[v]]]]]]]]]] [-d remote_addr] [-p remote_port]  [-t packet_timeout] [-T track_size]\n\
-\t-h : display this help and exit\n\
-\t-V : display version and exit\n\
-\t-D : daemonize\n\
-\t-k : use specified file as key file\n\
-\t-c : use specified file as cert file\n\
-\t-a : use specified file as ca file (strict checking is done if selected) (default: none)\n\
-\t-n : use specified string as the needed DN of nuauth (inforce certificate checking) (default: none)\n\
-\t-v : increase debug level (+1 for each 'v') (max useful number : 10)\n\
-\t-m : mark packet with userid (needed for connection expiration)\n"
+\t-h: display this help and exit\n\
+\t-V: display version and exit\n\
+\t-D: daemonize\n\
+\t-k: use specified file as key file\n\
+\t-c: use specified file as cert file\n\
+\t-a: use specified file as ca file (strict checking is done if selected) (default: none)\n\
+\t-n: use specified string as the needed DN of nuauth (inforce certificate checking) (default: none)\n\
+\t-v: increase debug level (+1 for each 'v') (max useful number: 10)\n\
+\t-m: mark packet with userid (needed for connection expiration)\n"
 #ifdef HAVE_LIBCONNTRACK
-"\t-C : listen to conntrack events (needed for connection expiration)\n\
-\t-M : only report event on marked connections to nuauth (implies -C and -m)\n"
+"\t-C: listen to conntrack events (needed for connection expiration)\n\
+\t-M: only report event on marked connections to nuauth (implies -C and -m)\n"
 #endif
-"\t-d : remote address we send auth requests to (adress of the nuauth server) (default : 127.0.0.1)\n\
-\t-p : remote port we send auth requests to (TCP port nuauth server listens on) (default : 4128)\n"
+"\t-d: remote address we send auth requests to (address of the nuauth server) (default: 127.0.0.1)\n\
+\t-p: remote port we send auth requests to (TCP port nuauth server listens on) (default: 4128)\n"
 #if USE_NFQUEUE
-		"\t-q : use nfqueue number (default : 0)\n"
+"\t-q: use nfqueue number (default: 0)\n"
 #endif
-                "\t-t : timeout to forget about packets when they don't match (default : 15 s)\n\
-\t-T : track size (default : 1000)\n",PACKAGE_TARNAME);
+"\t-t: timeout to forget about packets when they don't match (default: 15 s)\n\
+\t-T: track size (default: 1000)\n",PACKAGE_TARNAME);
 
             exit(EXIT_SUCCESS);
         }
