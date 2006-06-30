b33b05638252fdc7ff19e22c543173a126e7f2e1
Victor Stinner <victor.stinner@inl.fr>
2006-06-30 12:39:34 +0000

    Add copyright header

--- src/include/packet_parser.c
+++ src/include/packet_parser.c
@@ -1,3 +1,28 @@
+/*
+ ** Copyright(C) 2003-2006 Eric Leblond <regit@inl.fr>
+ **                        INL http://www.inl.fr/
+ **
+ ** This program is free software; you can redistribute it and/or modify
+ ** it under the terms of the GNU General Public License as published by
+ ** the Free Software Foundation, version 2 of the License.
+ **
+ ** This program is distributed in the hope that it will be useful,
+ ** but WITHOUT ANY WARRANTY; without even the implied warranty of
+ ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ ** GNU General Public License for more details.
+ **
+ ** You should have received a copy of the GNU General Public License
+ ** along with this program; if not, write to the Free Software
+ ** Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+/** \file pckt_authsrv.c
+ *  \brief Functions to parse a network packet
+ * 
+ * Functions fill ::tracking_t structure fields. Parser are: IPv4, IPv6, UDP,
+ * TCP, ICMP and ICMP6.
+ */
+
 #include "packet_parser.h"
 
 #include <netinet/tcp.h>
