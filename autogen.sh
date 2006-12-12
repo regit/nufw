5e82ee52140a4c5fa6d70c3254748ac6af84c41e
Victor Stinner <victor.stinner@inl.fr>
2006-12-12 16:23:59 +0000

    * Don't use automake 1.4 anymore!  * Add function to choose the best version of each autotool

--- autogen.sh
+++ autogen.sh
@@ -1,20 +1,74 @@
 #!/bin/sh
 
-libtoolize --force --automake
+echo "[+] Run libtoolize"
+libtoolize --force --automake || exit $?
 
-echo "Run aclocal"
-aclocal -I macros/
+#-----------------------------------------------------------------------------
 
-echo "Run autoheader"
-autoheader
+find_tool_version()
+{
+    TOOL=$1
+    MINVER=$2
+    OKVER=$3
+    NOKREG=$4
 
-echo "Run autoconf"
-autoconf
+    for i in ${OKVER}; do
+        if ${TOOL}${i} --version > /dev/null 2>&1; then
+            echo "${TOOL}${i}"
+            exit
+        fi
+    done
+    if ${TOOL} --version > /dev/null 2>&1; then
+        case "$(${TOOL} --version | sed -e '1s/[^0-9]*//' -e q)" in
+            ${NOKREG})
+            echo "You need ${TOOL} version (at least) ${MINVER} !" 1>&2
+            exit 1;;
+        esac
+    else
+        echo "You need ${TOOL} !" 1>&2
+        exit 1
+    fi
+    echo "${TOOL}"
+}
 
-echo "Run automake"
-automake -a --copy
+#-----------------------------------------------------------------------------
 
-echo "Done."
+AUTOMAKE_MIN_VERSION="1.6"
+AUTOMAKE_OK_VERSIONS="-1.9 19 -1.8 18 -1.7 17 -1.6 16 -1.5 15"
+AUTOMAKE_NOK_REGEXP='0|0.*|1|1.[0-5]*'
+
+AUTOCONF_MIN_VERSION="2.58"
+AUTOCONF_OK_VERSIONS="258 259"
+AUTOCONF_NOK_REGEXP='0|0.*|1|1.*|2|2.[0-4]*|2.5[0-7]*'
+
+AUTOMAKE=$(find_tool_version automake "${AUTOMAKE_MIN_VERSION}" \
+                                      "${AUTOMAKE_OK_VERSIONS}" \
+                                      "${AUTOMAKE_NOK_REGEXP}") || exit 1
+ACLOCAL=$(find_tool_version aclocal "${AUTOMAKE_MIN_VERSION}" \
+                                    "${AUTOMAKE_OK_VERSIONS}" \
+                                    "${AUTOMAKE_NOK_REGEXP}") || exit 1
+AUTOCONF=$(find_tool_version autoconf "${AUTOCONF_MIN_VERSION}" \
+                                      "${AUTOCONF_OK_VERSIONS}" \
+                                      "${AUTOCONF_NOK_REGEXP}") || exit 1
+AUTOHEADER=$(find_tool_version autoheader "${AUTOCONF_MIN_VERSION}" \
+                                          "${AUTOCONF_OK_VERSIONS}" \
+                                          "${AUTOCONF_NOK_REGEXP}") || exit 1
+
+#-----------------------------------------------------------------------------
+
+echo "[+] Run aclocal"
+$ACLOCAL -I macros || exit $?
+
+echo "[+] Run autoheader"
+$AUTOHEADER || exit $?
+
+echo "[+] Run autoconf"
+$AUTOCONF || exit $?
+
+echo "[+] Run automake"
+$AUTOMAKE --add-missing --copy || exit $?
+
 echo
 echo "Now type: ./configure"
 echo "Help with: ./configure --help"
+
