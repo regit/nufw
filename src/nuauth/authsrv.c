efeaf74ca40394a19176e8832648436be29ba129
Eric Leblond <eleblond@inl.fr>
2007-02-07 09:06:29 +0000

    Add svn keyword Date and Revision.

--- src/nuauth/authsrv.c
+++ src/nuauth/authsrv.c
@@ -1,6 +1,9 @@
 /*
  ** Copyright(C) 2004-2006 INL
  ** Written by Eric Leblond <regit@inl.fr>
+ ** 
+ ** $Revision$
+ ** $Date$
  **
  ** This program is free software; you can redistribute it and/or modify
  ** it under the terms of the GNU General Public License as published by
