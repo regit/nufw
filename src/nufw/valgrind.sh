b9c8d397067a4c4c44996e5917a54be8ed910ef6
Victor Stinner <victor.stinner@inl.fr>
2007-03-29 13:24:55 +0000

    nufw/valgrind.sh:  * Fix shebang (always use bash)  * Use libc6-dbg when it's available  * Use command line argument for nufw arguments

--- src/nufw/valgrind.sh
+++ src/nufw/valgrind.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 echo "Run nuauth in Valgrind"
 echo
@@ -7,25 +7,33 @@ echo "!!! Use NuAuth with plaintext auth"
 echo
 
 LOG=valgrind.log
-NUFW_OPT=-vv
 
 function stop_valgrind
 {
-    echo "NuAuth in Valgrind stopped with CTRL+C"
+    echo "Interrupt nufw (in Valgrind) with CTRL+C."
     echo "Output written in file $LOG"
-}    
+}
 
 trap stop_valgrind SIGINT SIGTERM
-    
+
 # Some interesting options:
 #    --gen-suppressions=yes
 #    --gen-suppressions=yes \
 
 # Explains:
 #   --run-libc-freeres=no: Valgrind free all memory that libc allocates
- 
+
 # Disabled:
 #    --suppressions=valgrind.supp \
+
+if [ -d /usr/lib/debug ]; then
+   export LD_LIBRARY_PATH=/usr/lib/debug:$LD_LIBRARY_PATH
+   if [ -e /usr/lib/debug/libdl-2.4.so ]; then
+      export LD_PRELOAD=/usr/lib/debug/libdl-2.4.so
+   fi
+else
+   echo "VALGRIND WARNING: /usr/lib/debug directory is missing, install libc6-dbg"
+fi
 
 sudo valgrind \
     --show-reachable=yes -v \
@@ -33,8 +41,9 @@ sudo valgrind \
     --run-libc-freeres=yes \
     --leak-check=full \
     --verbose \
-    ./nufw $NUFW_OPT 2>&1
+    ./nufw "$@" 2>&1
 
 trap - SIGINT SIGTERM
 
 echo "Quit valgrind.sh"
+
