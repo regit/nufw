fe013c306155af5f0c989c5f3a01998d514431a1
Victor Stinner <victor.stinner@inl.fr>
2007-05-11 14:05:47 +0000

    tests/common.py: fix startNufw(): restart nufw if command line arguments changed

--- tests/common.py
+++ tests/common.py
@@ -21,7 +21,11 @@ def startNufw(args=None):
     """
     global _nufw
     if _nufw:
-        if args:
+        if args or _nufw.args:
+            # if comment line arguments changed: restart nufw!
+            print "RESTART NUFW"
+            print "args is %s" % repr(args)
+            print "_nufw.args was %s" % repr(_nufw.args)
             _stopNufw()
         else:
             return _nufw
