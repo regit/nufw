ed8ed6f9971a1a66d975cba03656a951c428b5a3
Eric Leblond <eleblond@inl.fr>
2007-05-12 18:58:50 +0000

    r3300@ghlodit:  regit | 2007-05-12 12:59:36 +0200  Add mark_flag module:   * Use 16 bits of the mark to push the flag   * Acl can now use this property to fix mark on a connection

---
+++ src/nuauth/modules/mark_flag/mark_flag.c
@@ -0,0 +1,60 @@
+/*
+** Copyright(C) 2007 INL
+**          written by Eric Leblond <regit@inl.fr>
+**
+** $Id$
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; version 2 of the License.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+*/
+
+
+#include <auth_srv.h>
+
+/**
+ * \ingroup NuauthModules
+ */
+
+/**
+ * @{ */
+
+/*
+ * Returns version of nuauth API
+ */
+G_MODULE_EXPORT uint32_t get_api_version()
+{
+	return NUAUTH_API_VERSION;
+}
+
+G_MODULE_EXPORT gboolean unload_module_with_params(gpointer params_p)
+{
+	return TRUE;
+}
+
+G_MODULE_EXPORT gboolean init_module_from_conf(module_t * module)
+{
+	log_message(VERBOSE_DEBUG, DEBUG_AREA_MAIN,
+		    "Mark_flag module ($Revision$)");
+	return TRUE;
+}
+
+G_MODULE_EXPORT nu_error_t finalize_packet(connection_t * connection,
+					   gpointer params)
+{
+	connection->mark =
+	    (connection->flag & 0xffff0000) | (connection->
+					      mark & 0xFFFF);
+	return NU_EXIT_OK;
+}
+
+/** @} */
