12f96ee4d7c0d447dbfbbbd531c7b5ed5f2b8476
Eric Leblond <eleblond@inl.fr>
2007-05-13 21:01:25 +0000

    r3321@ghlodit:  regit | 2007-05-13 21:14:59 +0200  Set tls variable in nuauth config to correct value to avoid a reject of  connection from nuauth and not from nutcpc.

--- tests/test_cert.py
+++ tests/test_cert.py
@@ -3,12 +3,16 @@ from unittest import TestCase, main
 from sys import stderr
 from common import createClient, connectClient
 from nuauth import Nuauth
+from nuauth_conf import NuauthConf
 from config import config
 from os.path import join as path_join
 
 class TestClientCert(TestCase):
     def setUp(self):
-        self.nuauth = Nuauth()
+        nuconfig = NuauthConf()
+        nuconfig["nuauth_tls_auth_by_cert"] = "0"
+        nuconfig["nuauth_tls_request_cert"] = "0"
+        self.nuauth = Nuauth(nuconfig)
 
     def tearDown(self):
         self.client.stop()
