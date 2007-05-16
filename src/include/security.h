dfeb9273772e3575f1c8d5e2a3e30caa916d3fb3
Victor Stinner <victor.stinner@inl.fr>
2007-05-16 09:27:47 +0000

    Ooops, also add new macro: SECURE_STRNCAT

--- src/include/security.h
+++ src/include/security.h
@@ -26,12 +26,23 @@
 /** \def SECURE_STRNCPY(dst,src,size)
  * Copy string src to dst. Copy at maximum size-1 characters and make
  * sure that the string finish with a '\\0'.
- * 
+ *
  * Workaround strncpy security problem: if size is smaller than strlen(src),
  * dst doesn't contains '\\0'. This macro copy on maximum size-1 characters,
  * and always write a '\\0' on last position (dst[size-1]).
  */
 #define SECURE_STRNCPY(dst, src, size) \
     do { strncpy(dst, src, (size)-1); (dst)[(size)-1] = '\0'; } while (0)
+
+/** \def SECURE_STRNCAT(dst,src,size)
+ * Copy string src to dst. Copy at maximum size-1 characters and make
+ * sure that the string finish with a '\\0'.
+ *
+ * Workaround strncat security problem: if size is smaller than strlen(src),
+ * dst doesn't contains '\\0'. This macro copy on maximum size-1 characters,
+ * and always write a '\\0' on last position (dst[size-1]).
+ */
+#define SECURE_STRNCAT(dst, src, size) \
+    do { strncat(dst, src, (size)-1); (dst)[(size)-1] = '\0'; } while (0)
 
 #endif				/* of ifndef _NUFW_SECURITY_H */
