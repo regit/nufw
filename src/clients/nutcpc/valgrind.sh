46e568524a065faf84a4cd4b097f573db2fb9ba9
Victor Stinner <victor.stinner@inl.fr>
2007-06-22 15:53:29 +0000

    Fix shebang (use bash); set G_SLICE and use /usr/lib/debug if available; use libc-freerers

--- src/clients/nutcpc/valgrind.sh
+++ src/clients/nutcpc/valgrind.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 NUTCPC=.libs/nutcpc
 NUTCPC_ARGS="-H localhost -U haypo -d"
@@ -12,7 +12,7 @@ function stop_valgrind
 {
     echo "NuAuth in Valgrind stopped with CTRL+C"
     echo "Output written in file $LOG"
-}    
+}
 
 USER=haypo
 NUAUTH_PATH=/home/haypo/inl/trunk
@@ -20,19 +20,24 @@ CLIENT=$NUAUTH_PATH/src/clients
 export LD_LIBRARY_PATH=$CLIENT/lib/.libs
 
 trap stop_valgrind SIGINT SIGTERM
-    
-# Some interesting options:
-#    --gen-suppressions=yes
-#    --gen-suppressions=yes \
 
-# Explains:
-#   --run-libc-freeres=no: Valgrind free all memory that libc allocates
+export G_SLICE=always-malloc
 
+if [ -d /usr/lib/debug ]; then
+   export LD_LIBRARY_PATH=/usr/lib/debug:$LD_LIBRARY_PATH
+   if [ -e /usr/lib/debug/libdl-2.4.so ]; then
+      export LD_PRELOAD=/usr/lib/debug/libdl-2.4.so
+   fi
+else
+   echo "VALGRIND WARNING: /usr/lib/debug directory is missing, install libc6-dbg"
+fi
+
+echo "Run \"$NUTCPC $NUTCPC_ARGS\"..."
 valgrind \
     --show-reachable=yes -v \
     --suppressions=valgrind.supp \
     --log-file-exactly=$LOG \
-    --run-libc-freeres=no \
+    --run-libc-freeres=yes \
     --leak-check=full \
     --verbose \
     $NUTCPC $NUTCPC_ARGS 2>&1
