3c24e2434810155ed753d57cdaad86e9358bd22e
Eric Leblond <eleblond@inl.fr>
2007-06-29 07:50:36 +0000

    Directly include nufw_source.H.

--- src/clients/nutcpc/nutcpc.c
+++ src/clients/nutcpc/nutcpc.c
@@ -20,6 +20,8 @@
  ** Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  */
 
+#include "nufw_source.h"
+
 #include "../lib/nuclient.h"
 #include <locale.h>
 #include <config.h>
