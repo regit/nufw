5426879efffd9daa9ae5f0d0d29b36c720b3703a
Pierre Chifflier <chifflier@inl.fr>
2008-11-14 15:31:03 +0000

    Fix warning (undeclared function)

--- src/clients/lib/nuclient.h
+++ src/clients/lib/nuclient.h
@@ -160,6 +160,9 @@ int nu_client_set_krb5_service(nuauth_session_t * session,
 int nu_client_set_ca_suppress_warning(nuauth_session_t * session,
 		int suppress_ca_warning);
 
+int nu_client_set_fqdn_suppress_verif(nuauth_session_t * session,
+		int suppress_fqdn_verif);
+
 int nu_client_connect(nuauth_session_t * session,
 		const char *hostname,
 		const char *service,
