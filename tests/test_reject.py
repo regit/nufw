6500080bd32e92dbf2aaf7b5ada191c4f9534339
Pierre Chifflier <chifflier@inl.fr>
2008-11-20 15:19:15 +0000

    Fix test_reject

--- tests/test_reject.py
+++ tests/test_reject.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 from unittest import TestCase, main
-from common import startNufw, connectClient
+from common import startNufw, connectClient; createClient, createClientWithCerts
 from nuauth import Nuauth
 from nuauth_conf import NuauthConf
 from inl_tests.iptables import Iptables
@@ -8,6 +8,7 @@ from filter import testAllowPort, testPortFailure, VALID_PORT
 from test_plaintext_auth import USERDB
 from plaintext import PlaintextAcl
 from errno import ETIMEDOUT, ENETUNREACH, EISCONN
+from os.path import abspath
 
 class TestICMPReject(TestCase):
     def setUp(self):
@@ -34,7 +35,7 @@ class TestICMPReject(TestCase):
         self.config["nuauth_reject_authenticated_drop"] = "0"
         self.nuauth = Nuauth(self.config)
         user = self.users[0]
-        client = user.createClient()
+        client = user.createClientWithCerts()
         testPortFailure(self, self.iptables, client, VALID_PORT, ETIMEDOUT)
         client.stop()
 
@@ -49,7 +50,7 @@ class TestICMPReject(TestCase):
         self.config["nuauth_reject_authenticated_drop"] = 1
         self.nuauth = Nuauth(self.config)
         user = self.users[0]
-        client = user.createClient()
+        client = user.createClientWithCerts()
         testPortFailure(self, self.iptables, client, VALID_PORT, ENETUNREACH)
         client.stop()
 
