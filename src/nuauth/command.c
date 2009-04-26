ddc96d07d0c6538d19dd8c10343d05e1e5f35862
Eric Leblond <eleblond@inl.fr>
2009-04-26 11:22:11 +0200

    nuauth_command: add "refresh crl" command
    This patchs is a port of the "refresh crl" option introduced in nufw-2.2.

--- src/nuauth/command.c
+++ src/nuauth/command.c
@@ -47,6 +47,7 @@ const char* COMMAND_HELP =
 "firewalls: list connected nufw firewalls\n"
 "packets count: display number of decision waiting packets\n"
 "refresh cache: refresh all caches\n"
+"refresh crl: refresh the TLS crl file\n"
 "disconnect ID: disconnect an user with his session identifier\n"
 "disconnect all: disconnect all users\n"
 "uptime: display nuauth starting time and uptime\n"
@@ -363,8 +364,10 @@ void command_execute(command_t * this, char *command)
 		nuauth_config_table_print(encoder, conf_server_side_print);
 	} else if (strcmp(command, "disconnect all") == 0) {
 		ok = command_disconnect_all(this, encoder);
+		force_refresh_crl_file();
 	} else if (strncmp(command, "disconnect ", 10) == 0) {
 		ok = command_disconnect(this, encoder, command+10);
+		force_refresh_crl_file();
 	} else if (strcmp(command, "reload") == 0) {
 		gboolean restart = nuauth_reload(0);
 		if (restart) {
@@ -382,6 +385,9 @@ void command_execute(command_t * this, char *command)
 		} else {
 			encoder_add_string(encoder, "Cache disabled");
 		}
+	} else if (strcmp(command, "refresh crl") == 0) {
+		force_refresh_crl_file();
+		encoder_add_string(encoder, "Refresh of CRL file done");
 	} else if (strncmp(command, "debug_level ", 12) == 0) {
 		int debug_level = atoi(command+12);
 		if ((0 < debug_level) && (debug_level <= 9)) {
