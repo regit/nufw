41283825d502b768d4e8fe109e205bd0a59a6376
Laurent Defert <lds@edenwall.com>
2010-04-09 09:48:45 +0200

    Add "refresh crl" command to nuauth_command

--- scripts/nuauth_command/nuauth_command/cmdline.py
+++ scripts/nuauth_command/nuauth_command/cmdline.py
@@ -3,13 +3,13 @@ from nuauth_command import Client, NuauthError
 import re
 from command_dec import Answer
 
-COMMANDS_COMPLETION = ("version", "confdump", "users", "refresh cache",
+COMMANDS_COMPLETION = ("version", "confdump", "users", "refresh cache", "refresh crl",
     "disconnect", "uptime", "reload", "help", "quit",
     "display debug_level", "display debug_areas", "debug_level",
     "debug_areas", "firewalls", "packets count", "reload periods", "user count", "display threads")
 
 COMMANDS_REGEX = re.compile(
-    "^(?:version|confdump|users|firewalls|refresh cache|nupik!|display debug_(?:level|areas)|"
+    "^(?:version|confdump|users|firewalls|refresh cache|refresh crl|nupik!|display debug_(?:level|areas)|"
     "debug_level [0-9]+|debug_areas [0-9]+|"
     "disconnect (?:.*)|uptime|reload(?: periods)?|help|quit|packets count|user count|display threads)$")
 
